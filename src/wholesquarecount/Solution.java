f86c4dbc82c9b9039c2ee30ff18febddafe00c24
Viktoriia <vikyautumn@mail.ru>
2018-06-21 11:07:54 +0300

    Added comments

--- src/wholesquarecount/Solution.java
+++ src/wholesquarecount/Solution.java
@@ -1,12 +1,15 @@
 package wholesquarecount;
 
 public class Solution {
+    // doesn't count that 0 is root for 0
     public static int solution(int A, int B){
+        //if both numbers are negative - there are no roots
         if (B <= 0)
-            return 0;
+            return 0; 
         
-        int res = 0, a = 0, b;        
+        int res = 0, a = 0, b;      
         if (A > 0){
+            //substraction decrease the result
             res = 1;
             a = (int) Math.ceil(Math.sqrt((double) A));
         }        
